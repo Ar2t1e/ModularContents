dd6d25326513cccd876c690ebdedefbcb8a59fc0
Ar2t1e <151051360+Ar2t1e@users.noreply.github.com>
2026-08-07 17:25:46 +0800

    🛠️Снёс Мусор

--- fix_blocks.sh
+++
@@ -1,1 +0,0 @@
-sed -i 's/this.setRegistryName(info.id);/this.setRegistryName("modularcontents", info.id);/g' src/main/java/modularcontents/custom/block/*.java
--- run_gradle.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-try { execSync('taskkill /F /IM java.exe'); } catch(e){}
-try {
-    execSync('gradlew build', {stdio: 'inherit'});
-    console.log("Success");
-} catch(e) {
-    console.log("Failed");
-}
